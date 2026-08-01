e0416975ff1687a327f890c4e78bd9732142886a
sakurakugu <171350650@qq.com>
2026-08-01 14:31:47 +0800

    新增: 客户端命令系统支持状态查询、附近放置与光照显示设置

--- forge/src/main/java/com/sakurakugu/autotorch/forge/AutoTorchForgeClient.java
+++ forge/src/main/java/com/sakurakugu/autotorch/forge/AutoTorchForgeClient.java
@@ -1,6 +1,7 @@
 package com.sakurakugu.autotorch.forge;
 
 import com.sakurakugu.autotorch.client.AutoTorchClient;
+import com.sakurakugu.autotorch.client.AutoTorchClientCommands;
 import com.sakurakugu.autotorch.client.LightOverlayRenderer;
 import com.sakurakugu.autotorch.client.SelectionRenderer;
 import com.sakurakugu.autotorch.network.PlatformNetworking;
