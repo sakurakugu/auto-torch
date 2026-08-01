b5b574b37313b85e7e8c370eee5e5793ed84c178
sakurakugu <171350650@qq.com>
2026-08-01 17:27:24 +0800

    适配: 为 Fabric 1.15.2 接入客户端命令分发

--- fabric/src/main/java/com/sakurakugu/autotorch/fabric/AutoTorchFabricClient.java
+++ fabric/src/main/java/com/sakurakugu/autotorch/fabric/AutoTorchFabricClient.java
@@ -13,7 +13,6 @@ import com.sakurakugu.autotorch.network.TaskStatusPayload;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
@@ -59,7 +58,6 @@ public final class AutoTorchFabricClient implements ClientModInitializer {
         KeyBindingHelper.registerKeyBinding(AutoTorchClient.OPEN_SCREEN);
         KeyBindingHelper.registerKeyBinding(AutoTorchClient.TOGGLE_LIGHT_OVERLAY);
         ClientTickEvents.END_CLIENT_TICK.register(minecraft -> client.tick());
-        AutoTorchClientCommands.register(ClientCommandManager.DISPATCHER);
 
         AttackBlockCallback.EVENT.register((player, level, hand, pos, direction) -> {
             if (client.onLeftClick(level, player.getItemInHand(hand), pos, true)) {
