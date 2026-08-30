16ebe10d0ce7e7889e4508ab498f2276aac4fe8f
sakurakugu <171350650@qq.com>
2026-08-30 11:55:41 +0800

    修复: 光照显示使用原版刷怪规则判定并移除树叶特判

--- common/src/main/java/com/sakurakugu/autotorch/client/LightOverlayState.java
+++ common/src/main/java/com/sakurakugu/autotorch/client/LightOverlayState.java
@@ -18,9 +18,10 @@ import net.minecraft.tags.BiomeTags;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.level.NaturalSpawner;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.LevelChunk;
 
@@ -427,8 +428,11 @@ public final class LightOverlayState {
             return null;
         }
 
-        if (floor.getBlock() instanceof LeavesBlock
-                || !Block.isFaceFull(floor.getCollisionShape(level, floorPos), Direction.UP)) {
+        if (!Block.isFaceFull(floor.getCollisionShape(level, floorPos), Direction.UP)) {
+            return null;
+        }
+        if (!NaturalSpawner.isSpawnPositionOk(
+                SpawnPlacements.Type.ON_GROUND, level, feet, EntityType.ZOMBIE)) {
             return null;
         }
         int blockLight = level.getBrightness(LightLayer.BLOCK, feet);
