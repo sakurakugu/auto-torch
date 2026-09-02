9d5178bd298cdfbb37eb42129f878db2103132fc
sakurakugu <171350650@qq.com>
2026-09-02 22:05:41 +0800

    修复: 调整光照显示设置界面数字输入框高度

--- common/src/main/java/com/sakurakugu/autotorch/client/LightingScreen.java
+++ common/src/main/java/com/sakurakugu/autotorch/client/LightingScreen.java
@@ -335,21 +335,21 @@ public final class LightingScreen extends Screen {
     }
 
     private EditBox integerBox(int x, int y, int boxWidth, String value) {
-        EditBox box = new EditBox(font, x, y, boxWidth, 20, "");
+        EditBox box = new EditBox(font, x, y, boxWidth, 18, "");
         box.setMaxLength(9);
         box.setValue(value);
         return addButton(box);
     }
 
     private EditBox limitBox(int x, int y, int boxWidth, String value) {
-        EditBox box = new EditBox(font, x, y, boxWidth, 20, "");
+        EditBox box = new EditBox(font, x, y, boxWidth, 18, "");
         box.setMaxLength(4);
         box.setValue(value);
         return addButton(box);
     }
 
     private EditBox sizeBox(int x, int y, int boxWidth) {
-        EditBox box = new EditBox(font, x, y, boxWidth, 20, "");
+        EditBox box = new EditBox(font, x, y, boxWidth, 18, "");
         box.setMaxLength(9);
         return addButton(box);
     }
