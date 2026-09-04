5710a79e9632f1821f108c871d29946dcd3127ac
sakurakugu <171350650@qq.com>
2026-09-05 02:39:26 +0800

    修复: 透视完全无效的问题

--- fabric/src/main/java/com/sakurakugu/autotorch/fabric/AutoTorchFabricClient.java
+++ fabric/src/main/java/com/sakurakugu/autotorch/fabric/AutoTorchFabricClient.java
@@ -93,6 +93,7 @@ public final class AutoTorchFabricClient implements ClientModInitializer {
             SelectionRenderer.render(camera, poseStack, buffers);
             LightOverlayRenderer.render(camera, poseStack, buffers);
             // 自定义几何必须在当前相机模型视图仍有效时提交，不能留到共享缓冲区稍后冲刷。
+            LightOverlayRenderer.endBatches(buffers);
             buffers.endBatch(RenderType.lines());
             buffers.endBatch(AutoTorchRenderTypes.seeThroughLines());
             buffers.endBatch(SelectionRenderer.faceRenderType());
--- forge/src/main/java/com/sakurakugu/autotorch/forge/AutoTorchForgeClient.java
+++ forge/src/main/java/com/sakurakugu/autotorch/forge/AutoTorchForgeClient.java
@@ -120,6 +120,7 @@ final class AutoTorchForgeClient {
         var buffers = minecraft.renderBuffers().bufferSource();
         SelectionRenderer.render(camera, poseStack, buffers);
         LightOverlayRenderer.render(camera, poseStack, buffers);
+        LightOverlayRenderer.endBatches(buffers);
         buffers.endBatch(RenderType.lines());
         buffers.endBatch(AutoTorchRenderTypes.seeThroughLines());
         buffers.endBatch(SelectionRenderer.faceRenderType());
