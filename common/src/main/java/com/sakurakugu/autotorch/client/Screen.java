2f999c6035e34a8aaa36208ca6c83cfc1bcd2218
sakurakugu <171350650@qq.com>
2026-09-19 21:17:58 +0800

    新增: 第二轮补点间距按亮度要求自动收紧并提示不可达目标

--- common/src/main/java/com/sakurakugu/autotorch/client/Screen.java
+++ common/src/main/java/com/sakurakugu/autotorch/client/Screen.java
@@ -2,6 +2,7 @@ package com.sakurakugu.autotorch.client;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
@@ -23,7 +24,10 @@ abstract class Screen extends GuiScreen {
     public boolean isPauseScreen() { return true; }
     @Override public boolean doesGuiPauseGame() { return isPauseScreen(); }
     protected List<?> children() { return children; }
-    protected void renderTooltip(String text, int mouseX, int mouseY) { drawHoveringText(text, mouseX, mouseY); }
+    protected void renderTooltip(String text, int mouseX, int mouseY) {
+        // 1.12.2 的字符串重载不会拆分换行，先按行拆分再交给多行提示渲染。
+        drawHoveringText(Arrays.asList(text.split("\n")), mouseX, mouseY);
+    }
     public boolean mouseScrolled(double mouseX, double mouseY, double amount) { return false; }
     @Override public void handleMouseInput() throws IOException {
         super.handleMouseInput();
