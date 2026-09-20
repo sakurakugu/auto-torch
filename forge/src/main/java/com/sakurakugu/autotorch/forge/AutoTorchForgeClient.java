ab9de2f8fcc44cab573fc5c7ef0255be042032d3
sakurakugu <171350650@qq.com>
2026-09-20 18:49:27 +0800

    重构: 将光照显示改成跟随摄像机移动，以支持灵魂出窍等自由视角 mod

--- forge/src/main/java/com/sakurakugu/autotorch/forge/AutoTorchForgeClient.java
+++ forge/src/main/java/com/sakurakugu/autotorch/forge/AutoTorchForgeClient.java
@@ -90,7 +90,8 @@ final class AutoTorchForgeClient {
 
     private void onTick(TickEvent.ClientTickEvent event) {
         if (event.phase == TickEvent.Phase.END) {
-            client.tick();
+            Minecraft minecraft = Minecraft.getInstance();
+            client.tick(minecraft.gameRenderer.getMainCamera().getBlockPosition());
             if (!Minecraft.getInstance().options.keyAttack.isDown()) {
                 selectionClickPos = null;
             }
