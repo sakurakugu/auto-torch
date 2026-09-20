b3693a89b03271c1c08a59649fa6796310b04593
sakurakugu <171350650@qq.com>
2026-09-20 16:37:29 +0800

    新增: 光照显示开关按钮添加自由视角风险提示

--- common/src/main/java/com/sakurakugu/autotorch/client/LightingScreen.java
+++ common/src/main/java/com/sakurakugu/autotorch/client/LightingScreen.java
@@ -196,7 +196,9 @@ public final class LightingScreen extends Screen {
         lightOverlayButton = addButton(button(lightOverlayMessage(), button -> {
             LightOverlayState.toggle();
             lightOverlayButton.setMessage(lightOverlayMessage().getString());
-        }).bounds(left, 258, 108, 20).build());
+        }).bounds(left, 258, 108, 20)
+                .tooltip(new TranslatableComponent("screen.autotorch.light_overlay.tooltip"))
+                .build());
         Component lightOverlayResetMessage = new TranslatableComponent("screen.autotorch.reset");
         addButton(new ResetButton(resetButtonX(lightOverlayResetMessage), 242,
                 font.width(lightOverlayResetMessage.getString()), 16, lightOverlayResetMessage, panelLeft(), panelLeft() + 310,
