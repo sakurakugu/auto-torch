d43bad57eba3428a08b7d9cc151c7d498e83b547
sakurakugu <171350650@qq.com>
2026-09-20 21:25:42 +0800

    删除: 移除光照显示开关按钮的自由视角风险提示

--- common/src/main/java/com/sakurakugu/autotorch/client/LightingScreen.java
+++ common/src/main/java/com/sakurakugu/autotorch/client/LightingScreen.java
@@ -198,9 +198,7 @@ public final class LightingScreen extends Screen {
         lightOverlayButton = addButton(button(lightOverlayMessage(), button -> {
             LightOverlayState.toggle();
             lightOverlayButton.setMessage(lightOverlayMessage());
-        }).bounds(left, 258, 108, 20)
-                .tooltip(new TranslatableComponent("screen.autotorch.light_overlay.tooltip"))
-                .build());
+        }).bounds(left, 258, 108, 20).build());
         Component lightOverlayResetMessage = new TranslatableComponent("screen.autotorch.reset");
         addButton(new ResetButton(resetButtonX(lightOverlayResetMessage), 242,
                 font.width(lightOverlayResetMessage), 16, lightOverlayResetMessage, panelLeft(), panelLeft() + 310,
